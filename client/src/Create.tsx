26c29a6ddf0e9ddc5c5632ff6e10407efae80e72
stakbucks <dhassidu11@gmail.com>
2023-04-07 22:35:25 +0900

    이미지 url 서버 static 폴더로 접근해서 가져오기

--- client/src/Create.tsx
+++ client/src/Create.tsx
@@ -1,77 +1,11 @@
-import { useRef, useState } from "react";
-import ReactQuill from "react-quill";
-import Quill from "quill";
-import { ImageResize } from "quill-image-resize-module-ts";
+import { useMemo, useRef, useState } from "react";
+import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { createPost } from "./api/api";
 import { useNavigate } from "react-router-dom";
-
-Quill.register("modules/imageResize", ImageResize);
-
-const modules = {
-  toolbar: {
-    container: [
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      [{ font: [] }],
-      [{ align: [] }],
-
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [{ list: "ordered" }, { list: "bullet" }, "link"],
-      [
-        {
-          color: [
-            "#000000",
-            "#e60000",
-            "#ff9900",
-            "#ffff00",
-            "#008a00",
-            "#0066cc",
-            "#9933ff",
-            "#ffffff",
-            "#facccc",
-            "#ffebcc",
-            "#ffffcc",
-            "#cce8cc",
-            "#cce0f5",
-            "#ebd6ff",
-            "#bbbbbb",
-            "#f06666",
-            "#ffc266",
-            "#ffff66",
-            "#66b966",
-            "#66a3e0",
-            "#c285ff",
-            "#888888",
-            "#a10000",
-            "#b26b00",
-            "#b2b200",
-            "#006100",
-            "#0047b2",
-            "#6b24b2",
-            "#444444",
-            "#5c0000",
-            "#663d00",
-            "#666600",
-            "#003700",
-            "#002966",
-            "#3d1466",
-            "custom-color",
-          ],
-          handlers: {
-            image: imageHandler,
-          },
-        },
-        { background: [] },
-      ],
-      ["image", "video"],
-      ["clean"],
-    ],
-  },
-  imageResize: {},
-};
-
+import axios from "axios";
 function Create() {
-  const quillRef = useRef();
+  const quillRef = useRef<any>(null);
   const navigate = useNavigate();
   const [value, setValue] = useState("");
   const [titleValue, setTitleValue] = useState<string>("");
@@ -91,28 +25,79 @@ function Create() {
       console.log(error);
     }
   };
-  const imageHandler = () => {
-    const input = document.createElement('input');
 
-    input.setAttribute('type', 'file');
-    input.setAttribute('accept', 'image/*');
-    document.body.appendChild(input);
-    
-    input.click();
-  
-    input.onchange = async () => {
-      const [file] = input.files;
-      
-      // S3 Presigned URL로 업로드하고 image url 받아오기
-      const { preSignedPutUrl: presignedURL, readObjectUrl: imageURL } = (await getS3PresignedURL(file.name)).data;
-      await uploadImage(presignedURL, file);
-      
-      // 현재 커서 위치에 이미지를 삽입하고 커서 위치를 +1 하기
-      const range = quillRef.current.getEditorSelection();
-      quillRef.current.getEditor().insertEmbed(range.index, 'image', imageURL)
-      quillRef.current.getEditor().setSelection(range.index + 1);
-      document.body.querySelector(':scope > input').remove()
+  const imageHandler = () => {
+    console.log("에디터에서 이미지 버튼을 클릭하면 이 핸들러가 시작됩니다!");
+
+    // 1. 이미지를 저장할 input type=file DOM을 만든다.
+    const input = document.createElement("input");
+    // 속성 써주기
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click(); // 에디터 이미지버튼을 클릭하면 이 input이 클릭된다.
+    // input이 클릭되면 파일 선택창이 나타난다.
+
+    // input에 변화가 생긴다면 = 이미지를 선택
+    input.addEventListener("change", async () => {
+      console.log("온체인지");
+      const file = input.files?.[0];
+      // multer에 맞는 형식으로 데이터 만들어준다.
+      const formData = new FormData();
+      formData.append("img", file as any); // formData는 키-밸류 구조
+      // 백엔드 multer라우터에 이미지를 보낸다.
+      try {
+        const result = await axios.post("http://localhost:4000/img", formData);
+        console.log("성공 시, 백엔드가 보내주는 데이터", result.data.url);
+        const IMG_URL = result.data.url;
+        console.log(IMG_URL);
+        // 이 URL을 img 태그의 src에 넣은 요소를 현재 에디터의 커서에 넣어주면 에디터 내에서 이미지가 나타난다
+        // src가 base64가 아닌 짧은 URL이기 때문에 데이터베이스에 에디터의 전체 글 내용을 저장할 수있게된다
+        // 이미지는 꼭 로컬 백엔드 uploads 폴더가 아닌 다른 곳에 저장해 URL로 사용하면된다.
+
+        // 이미지 태그를 에디터에 써주기 - 여러 방법이 있다.
+        const editor = quillRef.current.getEditor(); // 에디터 객체 가져오기
+        console.log(editor);
+        // 1. 에디터 root의 innerHTML을 수정해주기
+        // editor의 root는 에디터 컨텐츠들이 담겨있다. 거기에 img태그를 추가해준다.
+        // 이미지를 업로드하면 -> 멀터에서 이미지 경로 URL을 받아와 -> 이미지 요소로 만들어 에디터 안에 넣어준다.
+        // editor.root.innerHTML =
+        //   editor.root.innerHTML + `<img src=${IMG_URL} /><br/>`; // 현재 있는 내용들 뒤에 써줘야한다.
+
+        // 2. 현재 에디터 커서 위치값을 가져온다
+        const range = editor.getSelection();
+        // 가져온 위치에 이미지를 삽입한다
+        editor.insertEmbed(range.index, "image", IMG_URL);
+      } catch (error) {
+        console.log("실패했어요ㅠ");
+      }
+    });
+  };
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          ["image"],
+          [{ header: [1, 2, 3, false] }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+        ],
+        handlers: {
+          // 이미지 처리는 우리가 직접 imageHandler라는 함수로 처리할 것이다.
+          image: imageHandler,
+        },
+      },
     };
+  }, []);
+  // 위에서 설정한 모듈들 foramts을 설정한다
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "image",
+  ];
 
   //여기에 에디터 넣을 예정
   console.log(value);
--- server/src/server.js
+++ server/src/server.js
@@ -2,16 +2,35 @@ import express from "express";
 import cors from "cors";
 import "./db";
 import Post from "./Post";
-
+const path = require("path");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    // 저장할 장소
+    destination(req, file, cb) {
+      cb(null, "public/uploads");
+    },
+    // 저장할 이미지의 파일명
+    filename(req, file, cb) {
+      const ext = path.extname(file.originalname); // 파일의 확장자
+      console.log("file.originalname", file.originalname);
+      // 파일명이 절대 겹치지 않도록 해줘야한다.
+      // 파일이름 + 현재시간밀리초 + 파일확장자명
+      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  // limits: { fileSize: 5 * 1024 * 1024 } // 파일 크기 제한
+});
 const app = express();
-
-app.use(express.json());
 
 const handleServerOpen = () => console.log("SERVER OPEN 👾");
 
 app.listen(4000, handleServerOpen);
 
 app.use(cors({ origin: "*", credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // 내부 url 파서 사용
+app.use(express.static("public"));
 
 app.get("/all-posts", async (req, res) => {
   const all = await Post.find({});
@@ -33,4 +52,16 @@ app.get(`/post/:id`, async (req, res) => {
 app.delete(`/delete-all`, async (req, res) => {
   await Post.deleteMany({});
   return;
+});
+
+app.post("/img", upload.single("img"), (req, res) => {
+  // 해당 라우터가 정상적으로 작동하면 public/uploads에 이미지가 업로드된다.
+  // 업로드된 이미지의 URL 경로를 프론트엔드로 반환한다.
+  console.log("전달받은 파일", req.file);
+  console.log("저장된 파일의 이름", req.file.filename);
+
+  // 파일이 저장된 경로를 클라이언트에게 반환해준다.
+  const IMG_URL = `http://localhost:4000/uploads/${req.file.filename}`;
+  console.log(IMG_URL);
+  res.json({ url: IMG_URL });
 });
